03b87b55b24c7e4d32f34aba0c6a0e7b5d62c42e
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-10-17 11:00:36 +0200

    Add public subnet

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,3 +1,12 @@
 resource "aws_vpc" "myvpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.myvpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "This is a public subnet"
+  }
+}
