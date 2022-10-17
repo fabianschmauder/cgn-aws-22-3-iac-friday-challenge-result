3e04cbf7485a0d6f38231b6fa78699ae5cb8fb78
Fabian Schmauder <fabian.schmauder@gmail.com>
2022-10-17 11:05:11 +0200

    Add public route table

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -14,3 +14,21 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.myvpc.id
 }
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.myvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Public route table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
